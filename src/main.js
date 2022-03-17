00b5f9d6d1f27b38c85e56291b7440c1d6fcad93
Demi Avaliani <demikoavaliani@gmail.com>
2022-03-17 20:10:33 +0200

    setup-api-calls / axios ready

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
+import axios from "axios";
+import VueAxios from "vue-axios";
 import "./assets/css/style.css";
 
 Vue.config.productionTip = false;
+Vue.use(VueAxios, axios);
 
 new Vue({
 	store,
---
+++ vue.config.js
@@ -0,0 +1,7 @@
+// vue.config.js
+module.exports = {
+	// options...
+	devServer: {
+		proxy: "https://homeassignment.scoro.com",
+	},
+};
